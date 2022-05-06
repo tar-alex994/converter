ef22df3f747dfd5360e90ea8b03b9f37d3d4d4d5
alex <alextar994@gmail.com>
2022-05-06 19:15:45 +0300

    Write routes for admin authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get    'session', to: "session#index"
+    post   'session', to: "session#create"
+    delete 'session', to: "session#destroy"
+  end
+  
   root 'converter#index'
 end
