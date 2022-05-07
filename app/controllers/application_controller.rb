5396cbb845afc0ad1bb6959a5748881a6f5393b5
alex <alextar994@gmail.com>
2022-05-07 14:54:12 +0300

    Add loging for any action of controller
    Before start work any action, application will log some parameters of request.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    before_action :add_log
+
+    private
+        def add_log
+            Log.create(
+                remote_ip:          request.remote_ip,
+                request_id:         request.request_id,
+                method:             request.method,
+                path:               request.path,
+                query_string:       request.query_string,
+                request_parameters: request.request_parameters 
+            )
+        end
 end
