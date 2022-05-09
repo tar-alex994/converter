e78e4c200026347d871c8e604443c59bdfe9fcd5
alex <alextar994@gmail.com>
2022-05-09 19:22:05 +0300

    Add pagination for logs from file

--- app/controllers/admin/logs_controller.rb
+++ app/controllers/admin/logs_controller.rb
@@ -1,9 +1,29 @@
 class Admin::LogsController < Admin::AdminController
-    def from_controllers
-        @logs = Log.all
-    end
+  def from_controllers
+      @logs = Log.all
+  end
 
-    def from_file
-        @logs = File.readlines Rails.application.root + "log/production.log"
+  def from_file
+    @current_page = current_page || 1
+    @logs         = File.readlines Rails.application.root + "log/production.log"
+    page_size     = 20
+    @last_page    = @logs.size / page_size + (@logs.size % page_size == 0 ? 0 : 1)
+
+    begin_log = (@current_page - 1) * page_size
+
+    if @current_page == 0
+      begin_log     = 0
+      @current_page = 1
+    elsif begin_log > @logs.size
+      begin_log     = @logs.size - (@logs.size % page_size)
+      @current_page = @last_page
+    end
+      
+    @logs = @logs[begin_log, page_size]
+  end
+
+  private
+    def current_page
+      params[:current_page]&.match(/\A\d+/)&.to_s&.to_i
     end
 end
