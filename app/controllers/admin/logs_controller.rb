c869b3a9027cf77767164d90cb4de9729de3a082
alex <alextar994@gmail.com>
2022-05-12 19:30:54 +0300

    Refactoring Admin::LogsController

--- app/controllers/admin/logs_controller.rb
+++ app/controllers/admin/logs_controller.rb
@@ -1,56 +1,35 @@
 class Admin::LogsController < Admin::AdminController
   def from_controllers
-    @logs = Log.order(:created_at).all
-    @logs = @logs.where("id = ?", params[:id])                                             if filter?(:id)
-    @logs = @logs.where("remote_ip LIKE ?", "%" + params[:ip] + "%")                       if filter?(:ip)
-    @logs = @logs.where("query_string LIKE ?", "%" + params[:get_parameters] + "%")        if filter?(:get_parameters)
-    @logs = @logs.where("method = ?", params[:method]&.upcase)                             if filter?(:method)
-    @logs = @logs.where("request_id = ?", "%" + params[:request_id] + "%")                 if filter?(:request_id)
-    @logs = @logs.where("path LIKE ?", "%" + params[:path] + "%")                          if filter?(:path) && params[:exact_match] == "0"
-    @logs = @logs.where("path = ?", params[:path])                                         if filter?(:path) && params[:exact_match] == "1"
-    @logs = @logs.where("request_parameters LIKE ?", "%" + params[:post_parameters] + "%") if filter?(:post_parameters)
+    @logs = Log.filtered_logs(params)
 
-    @current_page = current_page || 1
+    @current_page = current_page
     page_size     = 6
     log_count     = @logs.count
     @last_page    = log_count / page_size + (log_count % page_size == 0 && log_count > 0 ? 0 : 1)
     offset        = (@current_page - 1) * page_size
-    
-    if @current_page == 0
-      offset        = 0
-      @current_page = 1
-    elsif @current_page > @last_page
+
+    if @current_page > @last_page
       @current_page = @last_page
       offset        = (@current_page - 1) * page_size
     end
+    
     @logs = @logs.offset(offset).limit(page_size)
   end
 
   def from_controllers_all
-    @logs = Log.order(:created_at).all
-    @logs = @logs.where("id = ?", params[:id])                                             if filter?(:id)
-    @logs = @logs.where("remote_ip LIKE ?", "%" + params[:ip] + "%")                       if filter?(:ip)
-    @logs = @logs.where("query_string LIKE ?", "%" + params[:get_parameters] + "%")        if filter?(:get_parameters)
-    @logs = @logs.where("method = ?", params[:method]&.upcase)                             if filter?(:method)
-    @logs = @logs.where("request_id = ?", "%" + params[:request_id] + "%")                 if filter?(:request_id)
-    @logs = @logs.where("path LIKE ?", "%" + params[:path] + "%")                          if filter?(:path) && params[:exact_match] == "0"
-    @logs = @logs.where("path = ?", params[:path])                                         if filter?(:path) && params[:exact_match] == "1"
-    @logs = @logs.where("request_parameters LIKE ?", "%" + params[:post_parameters] + "%") if filter?(:post_parameters)
+    @logs = Log.filtered_logs(params)
   end
 
   def from_file
-    @current_page = current_page || 1
+    @current_page = current_page
     @logs         = File.readlines Rails.application.root + "log/production.log"
-    @logs         = log_filter(@logs)
+    @logs         = logs_filter(@logs)
     page_size     = 20
     @last_page    = @logs.size / page_size + (@logs.size % page_size == 0 ? 0 : 1)
 
     begin_log = (@current_page - 1) * page_size
 
-    if @current_page == 0
-      begin_log     = 0
-      @current_page = 1
-    elsif begin_log > @logs.size
+    if begin_log > @logs.size
       begin_log     = @logs.size - (@logs.size % page_size)
       @current_page = @last_page
     end
@@ -60,27 +39,21 @@ class Admin::LogsController < Admin::AdminController
 
   def from_file_all
     @logs = File.readlines(Rails.application.root + "log/production.log")
-    @logs = log_filter(@logs)
+    @logs = logs_filter(@logs)
   end
+
 
   private
     def current_page
-      params[:current_page]&.match(/\A\d+/)&.to_s&.to_i
+      page = (params[:current_page]&.match(/\A\d+/)&.to_s&.to_i || 1)
+      page == 0 ? 1 : page
     end
 
-    def log_filter logs
+    def logs_filter logs
       if params[:filter]
         logs.select { |log| log.downcase.include?(params[:filter].downcase) } 
       else
         logs
-      end
-    end
-
-    def filter? attr
-      if params[attr] && !(params[attr].strip.empty?)
-        true
-      else
-        false
       end
     end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,32 @@
 class Log < ApplicationRecord
+  def self.filtered_logs attributes
+    logs = Log.order(:created_at)
+
+    if attributes[:id] && !attributes[:id].strip.empty?
+      logs = logs.where("id = ?", attributes[:id])
+    end
+    if attributes[:ip] && !attributes[:ip].strip.empty?
+      logs = logs.where("remote_ip LIKE ?", "%" + attributes[:ip] + "%")
+    end
+    if attributes[:get_parameters] && !attributes[:get_parameters].strip.empty?
+      logs = logs.where("query_string LIKE ?", "%" + attributes[:get_parameters] + "%")
+    end
+    if attributes[:method] && !attributes[:method].strip.empty?
+      logs = logs.where("method = ?", attributes[:method]&.upcase)
+    end
+    if attributes[:request_id] && !attributes[:request_id].strip.empty?
+      logs = logs.where("request_id LIKE ?", "%" + attributes[:request_id] + "%")
+    end
+    if attributes[:path] && !attributes[:path].strip.empty?
+      logs = logs.where("path LIKE ?", "%" + attributes[:path] + "%")
+    end
+    if attributes[:path] && attributes[:exact_match] == "1" && !attributes[:path].strip.empty?
+      logs = logs.where("path = ?", attributes[:path])
+    end
+    if attributes[:post_parameters] && !attributes[:post_parameters].strip.empty?
+      logs = logs.where("request_parameters LIKE ?", "%" + attributes[:post_parameters] + "%")
+    end
+
+    logs
+  end
 end
